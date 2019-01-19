59528c718700f643e89f2fb6a6a54ecc193bc2ad
Mohammad Quadri <quadrixm@gmail.com>
2019-01-19 18:55:01 +0530

    Few fixes

---
+++ README.md
@@ -0,0 +1,6 @@
+## ResizeTo
+
+### Introduction
+
+ResizeTo is a sample project to resize an image to a given dimension
+
--- src/main/java/com/example/resizeTo/ImageUtil.java
+++ src/main/java/com/example/resizeTo/ImageUtil.java
@@ -111,10 +111,6 @@ public class ImageUtil {
                 BufferedImage.TYPE_INT_RGB);
         final Graphics2D graphics2D = bufferedImage.createGraphics();
         graphics2D.setComposite(AlphaComposite.Src);
-//        graphics2D.setBackground(Color.WHITE);
-//        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-//        graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
-//        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
         graphics2D.drawImage(image, 0, 0, dimension.width, dimension.height, null);
         graphics2D.dispose();
 
@@ -126,10 +122,6 @@ public class ImageUtil {
                 BufferedImage.TYPE_INT_ARGB);
         final Graphics2D graphics2D = bufferedImage.createGraphics();
         graphics2D.setComposite(AlphaComposite.Src);
-//        graphics2D.setBackground(Color.WHITE);
-//        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-//        graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
-//        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
         graphics2D.drawImage(image, initialPoint.x, initialPoint.y, imageDimen.width, imageDimen.height, null);
         graphics2D.dispose();
 
